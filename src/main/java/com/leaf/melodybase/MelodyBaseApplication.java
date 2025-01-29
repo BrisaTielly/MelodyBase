cc1194dcf72c25c8a2b391f87194a9f2e123eb52
BrisaTielly <brisatielly12@gmail.com>
2025-01-29 12:14:05 -0300

    first commit

---
+++ src/main/java/com/leaf/melodybase/MelodyBaseApplication.java
@@ -0,0 +1,13 @@
+package com.leaf.melodybase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodyBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MelodyBaseApplication.class, args);
+    }
+
+}
