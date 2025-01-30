22d3530ccf9b62bb1ce1ca57e8a7e88164ed9075
BrisaTielly <brisatielly12@gmail.com>
2025-01-29 22:12:54 -0300

    feat: rotas restantes de achar por id, salvar, atualizar e deletar para a classe artistas

--- src/main/java/com/leaf/melodybase/controllers/ArtistController.java
+++ src/main/java/com/leaf/melodybase/controllers/ArtistController.java
@@ -1,13 +1,10 @@
 package com.leaf.melodybase.controllers;
 
 import com.leaf.melodybase.dtos.ArtistDTO;
-import com.leaf.melodybase.repositories.ArtistRepository;
 import com.leaf.melodybase.services.ArtistService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +21,29 @@ public class ArtistController {
     public ResponseEntity<List<ArtistDTO>> findAll(){
         List<ArtistDTO> artistDTO = artistService.findAll();
         return ResponseEntity.status(HttpStatus.FOUND).body(artistDTO);
+    }
+
+    @GetMapping("/list/{id}")
+    public ResponseEntity<ArtistDTO> findById(@PathVariable Long id){
+        ArtistDTO artistDTO = artistService.findById(id);
+        return ResponseEntity.status(HttpStatus.FOUND).body(artistDTO);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> save (@RequestBody ArtistDTO artistDTO){
+        ArtistDTO artist = artistService.save(artistDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(artist);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update (@PathVariable Long id, @RequestBody ArtistDTO artistDTO){
+        ArtistDTO artist = artistService.update(id,artistDTO);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(artist);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> delete (@PathVariable Long id){
+        artistService.delete(id);
+        return ResponseEntity.ok("Música com o id " + id + " Deletada com sucesso!!!");
     }
 }
--- src/main/java/com/leaf/melodybase/services/ArtistService.java
+++ src/main/java/com/leaf/melodybase/services/ArtistService.java
@@ -1,13 +1,16 @@
 package com.leaf.melodybase.services;
 
 import com.leaf.melodybase.dtos.ArtistDTO;
+import com.leaf.melodybase.dtos.SongDTO;
 import com.leaf.melodybase.mappers.ArtistMapper;
 import com.leaf.melodybase.models.ArtistModel;
+import com.leaf.melodybase.models.SongModel;
 import com.leaf.melodybase.repositories.ArtistRepository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,6 +30,34 @@ public class ArtistService {
         return artists.stream()
                 .map(artistMapper::mapToDTO)
                 .collect(Collectors.toList());
+    }
+
+    public ArtistDTO findById(Long id){
+        Optional<ArtistModel> artistModel = artistRepository.findById(id);
+        return artistModel
+                .map(artistMapper::mapToDTO).orElse(null);
+    }
+
+    public ArtistDTO save(ArtistDTO artistDTO){
+        ArtistModel artistModel = artistMapper.mapToModel(artistDTO);
+        artistModel = artistRepository.save(artistModel);
+        return artistMapper.mapToDTO(artistModel);
+    }
+
+    public ArtistDTO update(Long id, ArtistDTO artistDTO) {
+        Optional<ArtistModel> artistModel = artistRepository.findById(id);
+        if(artistModel.isPresent()){
+            //Encontrou uma model com esse id
+            ArtistModel artistAtual = artistMapper.mapToModel(artistDTO); //Transformando em model
+            artistAtual.setId(id);
+            artistAtual = artistRepository.save(artistAtual);
+            return artistMapper.mapToDTO(artistAtual);
+        }
+        return null;
+    }
+
+    public void delete(Long id){
+        artistRepository.deleteById(id);
     }
 
 
